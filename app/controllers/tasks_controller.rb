4d48e841792f812ea8bd6ce5b390feacdab54b13
Catherine Tran & Ruben Rhodes <student@epicodus.com>
2014-09-04 11:57:17 -0700

    fix multiple bugs. Create routes, controllers and htmls for creating and updating tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,28 @@ class TasksController < ApplicationController
     @tasks = Task.all
     render('tasks/index.html.erb')
   end
+
+  def show
+    @task = Task.find(params[:id])
+    render = ('tasks/show.html.erb')
+  end
+
+  def new
+    @task = Task.new(params[:task])
+    render ('tasks/new.html.erb')
+  end
+
+  def create
+    @task = Task.create(params[:task])
+    @tasks = Task.all
+    render('tasks/index.html.erb')
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(:done => true)
+      @tasks = Task.all
+      render('tasks/index.html.erb')
+    end
+  end
 end
